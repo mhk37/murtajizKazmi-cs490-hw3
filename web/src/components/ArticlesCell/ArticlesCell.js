c42d883eb3bbe525d60f48f3ccfaa7a834006702
mhk37 <112417219+mhk37@users.noreply.github.com>
2023-02-02 12:30:52 -0500

    Initial commit

---
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -0,0 +1,31 @@
+import Article from 'src/components/Article'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+      user {
+        name
+      }
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const Success = ({ articles }) => {
+  return (
+    <div className="space-y-10">
+      {articles.map((article) => (
+       <Article article={article} key={article.id} summary={true} />
+      ))}
+    </div>
+  )
+}
